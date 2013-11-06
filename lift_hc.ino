239228c12bd400362f96a5b752eb0fdea5a51b81
Gonzalo Martinez <gmartz21@gmail.com>
2013-11-06 17:48:16 -0500

    initial commit

---
+++ lift_hc.ino
@@ -0,0 +1,24 @@
+/* Light sensors */
+#define LEFT_LIGHT_SENSOR   A0
+#define CENTER_LIGHT_SENSOR A1
+#define RIGHT_LIGHT_SENSOR  A2
+
+/* Proximity sensor */
+
+
+/* Motors PWM pins */
+#define LEVITATION_FAN   3
+#define LEFT_PROP_FANS   5
+#define LEFT_PROP_SERVO  6
+#define CENTER_PROP_FAN  9
+#define RIGHT_PROP_FANS  10
+#define RIGHT_PROP_SERVO 11
+
+
+void setup() {
+  /* set 
+}
+
+void loop() {
+  
+}
